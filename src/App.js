63f978df8e43c83101eafd79f370cb5cc10ddff2
Michael Dilley <mike.dilley@gmail.com>
2019-05-10 12:40:09 -0500

    Add TopBar mock content, import/render in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home'
+import TopBar from './Components/TopBar'
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Home />
     </div>
   );
---
+++ src/Components/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TopBar() {
+  return <div>
+          <a href="#home">Home</a>
+          <span> </span>
+          <a href="#projects">Projects</a>
+         </div>
+
+}
+
+export default TopBar;
