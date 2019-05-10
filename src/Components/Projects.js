8d6be1057cb89a5aa30c265eb9b8ff2581953985
Michael Dilley <mike.dilley@gmail.com>
2019-05-10 12:36:43 -0500

    Add Projects.js to render Project component, add mock text to Home component and import/add render to App component

---
+++ src/Components/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Projects() {
+  return <div>
+          <h1>These are my projects!</h1>
+          <Project name={"Test Project"} />
+         </div>
+}
+
+export default Projects;
