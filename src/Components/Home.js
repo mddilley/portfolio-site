ca1ba1aa254a527c4fc1ffb1c3bf331ceba29bd5
Michael Dilley <mike.dilley@gmail.com>
2019-05-10 16:35:06 -0500

    Modify text in Home div.desc and change h2 to h1, change h2 style to h1 in App.css, add a:visited, remove font-family from .wrapper and move to body in index.css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,14 +7,14 @@ function Home() {
          </div>
          <div className="bottom-right">
          <div className="desc">
-          I am a web developer who finds passion in actualizing products and learning to apply new technologies. I enjoy incremental improvement and value flexibility and adaptivity in the development process. 
+          I am a web developer who finds passion in actualizing products and learning to apply new technologies. I enjoy incremental improvement and value flexibility and adaptivity in the development process.
          </div><br/>
            <a href="https://github.com/mddilley"><i className="fab fa-github"></i></a><span>  | </span>
            <a href="https://www.linkedin.com/in/mike-dilley"><i className="fab fa-linkedin-in"></i></a><span>  | </span>
            <a href="https://www.instagram.com/mdilley/"><i className="fab fa-instagram"></i></a><span>  | </span>
            <a href="https://www.facebook.com/mddilley"><i className="fab fa-facebook-f"></i></a><span>  | </span>
            <a href="mailto:mike.dilley@gmail.com"><i className="far fa-envelope"></i></a>
-           <h2>/ / / / mike dilley</h2>
+           <h1>/ / / / mike dilley</h1>
          </div>
          </>
 }
