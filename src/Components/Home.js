3b22f923e0176209d4718a9a88bb56f6cd90c85e
Michael Dilley <mike.dilley@gmail.com>
2019-05-10 14:20:19 -0500

    Add contact/links to Home component, add FontAwesome CDN to index.html

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 function Home() {
-  return <h1>Hello, world!</h1>
+  return <div class="bottom-right">
+            <a href="https://github.com/mddilley"><i class="fab fa-github"></i></a><span>  | </span>
+            <a href="https://www.linkedin.com/in/mike-dilley"><i class="fab fa-linkedin-in"></i></a><span>  | </span>
+            <a href="https://www.instagram.com/mdilley/"><i class="fab fa-instagram"></i></a><span>  | </span>
+            <a href="https://www.facebook.com/mddilley"><i class="fab fa-facebook-f"></i></a><span>  | </span>
+            <a href="mailto:mike.dilley@gmail.com"><i class="far fa-envelope"></i></a>
+            <h2>/ / / / mike dilley</h2>
+          </div>
 }
 
 export default Home;
