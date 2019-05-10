51d7ad7f489fa5a76df157af691628ea798b8866
Michael Dilley <mike.dilley@gmail.com>
2019-05-10 15:09:29 -0500

    Change indentations in Home components, add fragment surrounding new div with headshot img, add .top-right and .img-circle to App.css from local project

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 function Home() {
-  return <div className="bottom-right">
-            <a href="https://github.com/mddilley"><i className="fab fa-github"></i></a><span>  | </span>
-            <a href="https://www.linkedin.com/in/mike-dilley"><i className="fab fa-linkedin-in"></i></a><span>  | </span>
-            <a href="https://www.instagram.com/mdilley/"><i className="fab fa-instagram"></i></a><span>  | </span>
-            <a href="https://www.facebook.com/mddilley"><i className="fab fa-facebook-f"></i></a><span>  | </span>
-            <a href="mailto:mike.dilley@gmail.com"><i className="far fa-envelope"></i></a>
-            <h2>/ / / / mike dilley</h2>
-          </div>
+  return <>
+         <div class="top-right">
+           <img src="/headshot2squared.png" alt="Mike Dilley headshot" class="img-circle" />
+         </div>
+         <div className="bottom-right">
+           <a href="https://github.com/mddilley"><i className="fab fa-github"></i></a><span>  | </span>
+           <a href="https://www.linkedin.com/in/mike-dilley"><i className="fab fa-linkedin-in"></i></a><span>  | </span>
+           <a href="https://www.instagram.com/mdilley/"><i className="fab fa-instagram"></i></a><span>  | </span>
+           <a href="https://www.facebook.com/mddilley"><i className="fab fa-facebook-f"></i></a><span>  | </span>
+           <a href="mailto:mike.dilley@gmail.com"><i className="far fa-envelope"></i></a>
+           <h2>/ / / / mike dilley</h2>
+         </div>
+         </>
 }
 
 export default Home;
