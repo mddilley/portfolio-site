05c64b17b1cf3f44bdafdf4ff3e0437f970337cb
Michael Dilley <mike.dilley@gmail.com>
2019-05-10 16:54:56 -0500

    Add .bottom to App.css, add mock Project components to Projects components

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Project from './Project'
 
 function Projects() {
-  return <div>
-          <h1>These are my projects!</h1>
-          <Project name={"Test Project"} />
+  return <div className="bottom">
+          <Project name={"Test Project #1"} />
+          <Project name={"Test Project #2"} />
+          <Project name={"Test Project #3"} />
          </div>
 }
 
