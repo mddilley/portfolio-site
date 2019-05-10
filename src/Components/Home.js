6a05fe4e87edd75e306f22d37b2b260a11bf7fa1
Michael Dilley <mike.dilley@gmail.com>
2019-05-10 16:14:37 -0500

    Add .desc to App.css, add text to div.desc in Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,9 @@ function Home() {
            <img src="/headshot2squared.png" alt="Mike Dilley headshot" className="img-circle" />
          </div>
          <div className="bottom-right">
+         <div className="desc">
+          I am a web developer who finds passion in actualizing products and learning to apply new technologies. I enjoy incremental improvement and value flexibility and adaptivity in the development process. 
+         </div><br/>
            <a href="https://github.com/mddilley"><i className="fab fa-github"></i></a><span>  | </span>
            <a href="https://www.linkedin.com/in/mike-dilley"><i className="fab fa-linkedin-in"></i></a><span>  | </span>
            <a href="https://www.instagram.com/mdilley/"><i className="fab fa-instagram"></i></a><span>  | </span>
