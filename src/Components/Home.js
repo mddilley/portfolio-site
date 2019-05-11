4d089ef34e72ad47c016e41857a44342d7ee17b7
Michael Dilley <mike.dilley@gmail.com>
2019-05-11 13:32:06 -0500

    Add #my-name to h1 in Home component, add animation to h1#my-name and @keyframes slideIn in App.css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,10 +12,10 @@ function Home() {
            <a href="https://github.com/mddilley"><i className="fab fa-github"></i></a><span>  | </span>
            <a href="https://www.linkedin.com/in/mike-dilley"><i className="fab fa-linkedin-in"></i></a><span>  | </span>
            <a href="https://www.instagram.com/mdilley/"><i className="fab fa-instagram"></i></a><span>  | </span>
-           <a href="https://www.goodreads.com/mddilley/"><i class="fab fa-goodreads"></i></a><span>  | </span>
+           <a href="https://www.goodreads.com/mddilley/"><i className="fab fa-goodreads"></i></a><span>  | </span>
            <a href="https://www.facebook.com/mddilley"><i className="fab fa-facebook-f"></i></a><span>  | </span>
            <a href="mailto:mike.dilley@gmail.com"><i className="far fa-envelope"></i></a>
-           <h1>/ / / / mike dilley</h1>
+           <h1 id="my-name">/ / / / mike dilley</h1>
          </div>
          </>
 }
