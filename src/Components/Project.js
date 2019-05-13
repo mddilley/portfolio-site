ba25970d9ab87db76fe6883487dc659965e4018e
Michael Dilley <mike.dilley@gmail.com>
2019-05-13 11:05:06 -0500

    Add project images to /public, mock Project component and add #img-project to App.css

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Project(props) {
   return <div>
-          <h3>{props.name}</h3>
+          <div>
+            <img className="img-project" src="/oculus.png" alt="Oculus" />
+          </div>
+          <div>
+            <h3>{props.name}</h3>
+          </div>
          </div>
 }
 
