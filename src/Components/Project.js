1e4845425b08a6b5b91f1c50cea34d5cc4bc4d09
Michael Dilley <mike.dilley@gmail.com>
2019-05-13 11:31:45 -0500

    Add h1.project-title to App.css and modify margins for .project-left and .project-right, add className project-title to Project component h1, add /src/Data folder and add fn projectData

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -6,7 +6,7 @@ function Project(props) {
             <img className="img-project" src="/oculus.png" alt="Oculus" />
           </div>
           <div className="project-right">
-            <h1>Oculus</h1>
+            <h1 className="project-title">Oculus</h1>
             <p>erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt arcu non sodales neque sodales ut etiam sit amet nisl purus in mollis nunc sed id semper risus in hendrerit gravida rutrum quisque non tellus</p>
           </div>
          </div>
