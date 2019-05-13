a06f9d0a210346193b653a7dbd0d46df8a3d4718
Michael Dilley <mike.dilley@gmail.com>
2019-05-13 16:55:07 -0500

    Change img alt attr to props.name in Project component, change path for favicon in index.html

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Project(props) {
   return <div className="project-container">
           <div className="project-left">
-            <a href={props.link}><img className="img-project" src={"/" + props.img} alt="Oculus" /></a>
+            <a href={props.link}><img className="img-project" src={"/" + props.img} alt={props.name} /></a>
           </div>
           <div className="project-right">
             <a href={props.link} className="project-link"><h1 className="project-title">{props.name}</h1></a>
